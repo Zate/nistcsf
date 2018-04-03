d983bb9de88a5b54eb314bca7949694bf20da313
Zate Berg <zate75@gmail.com>
2018-04-03 11:01:36 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# NIST CSF Reporting
+
+Takes a file full of data and builds a pretty reporting website from it.
---
+++ main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"log"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+// CheckErr to handle errors
+func CheckErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// GetGems makes a GET request to the API
+// func GetGems(uri string) (b []byte) {
+// 	c := &http.Client{}
+// 	r, err := http.NewRequest("GET", "https://raw.githubusercontent.com/brather1ng/RePoE/master/data/"+uri, nil)
+// 	CheckErr(err)
+// 	resp, err := c.Do(r)
+// 	CheckErr(err)
+// 	defer resp.Body.Close()
+// 	b, err = ioutil.ReadAll(resp.Body)
+// 	CheckErr(err)
+// 	return b
+// }
+
+// GetNIST reads the nist.json file
+func GetNIST(file string) (raw []byte) {
+	raw, err := ioutil.ReadFile(file)
+	CheckErr(err)
+	return raw
+}
+
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Nist is a placeholder type for the nist.json to parse it
+type Nist []interface{}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func contains(arr []string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
+	e := echo.New()
+	e.Static("/static", "static")
+	e.File("/favicon.ico", "favicon.ico")
+	e.File("/common.css", "common.css")
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET}, //echo.PUT, echo.POST, echo.DELETE},
+	}))
+
+	var b []byte
+	var n []Nist
+
+	b = GetNIST("nist.json")
+	json.Unmarshal(b, &n)
+	log.Println(b)
+	// buf := new(bytes.Buffer)
+
+	// var handler func([]byte, []byte, jsonparser.ValueType, int) error
+	// handler = func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
+	// 	skip := false
+	// 	rs, _ := jsonparser.GetString(value, "base_item", "release_state")
+	// 	dn, _ := jsonparser.GetString(value, "base_item", "display_name")
+	// 	sup, _ := jsonparser.GetBoolean(value, "is_support")
+	// 	mc, _ := jsonparser.GetBoolean(value, "active_skill", "is_manually_casted")
+	// 	tag, _, _, _ := jsonparser.Get(value, "active_skill", "types")
+	// 	_ = json.Unmarshal(tag, &tags)
+	// 	for _, t := range tags {
+	// 		//log.Println(t)
+	// 		switch t {
+	// 		case "vaal", "aura", "curse", "movement", "buff":
+	// 			skip = true
+	// 		}
+	// 	}
+	// 	if rs == "released" && sup == false && mc == true && skip == false && dn != "Portal" && dn != "Detonate Mines" && dn != "Decoy Totem" {
+	// 		//log.Printf("%v %v", dn, tags[0])
+	// 		gems = append(gems, dn)
+	// 	}
+
+	// 	return nil
+	// }
+	// jsonparser.ObjectEach(b, handler)
+
+	// numGems := len(gems)
+
+	// log.Println(numGems)
+
+	// renderer := &TemplateRenderer{
+	// 	templates: template.Must(template.ParseGlob("public/*.html")),
+	// }
+	// e.Renderer = renderer
+
+	// e.GET("/api", func(c echo.Context) error {
+	// 	num1 := 7
+	// 	scion := c.FormValue("scion")
+	// 	if scion == "no" {
+	// 		num1 = 6
+	// 	}
+	// 	choice, ascen, rndgem := GetNums(num1, numGems)
+	// 	gemImg := strings.Replace(gems[rndgem], " ", "_", -1)
+
+	// 	r := map[string]interface{}{
+	// 		"class":      Classes[choice].Class,
+	// 		"ascendency": Classes[choice].Ascension[ascen],
+	// 		"name":       randomdata.SillyName(),
+	// 		"gemImg":     gemImg,
+	// 		"gem":        gems[rndgem],
+	// 		"league":     randomdata.StringSample("SSF Hardcore Abyss", "Normal Hardcore Abyss", "SSF Softcore Abyss", "Normal Softcore Abyss", "SSF Hardcore Standard", "Normal Hardcore Standard", "SSF Softcore Standard", "Normal Softcore Standard"),
+	// 		// "ssf":        randomdata.StringSample("SSF", "Normal"),
+	// 		// "hc":         randomdata.StringSample("HardCore", "SoftCore"),
+	// 	}
+	// 	return c.JSON(http.StatusOK, r)
+	// })
+
+	// e.GET("/", func(c echo.Context) error {
+	// 	num1 := 7
+	// 	scion := c.FormValue("scion")
+	// 	if scion == "no" {
+	// 		num1 = 6
+	// 	}
+	// 	temp := "rnd.html"
+	// 	league := c.FormValue("league")
+	// 	if league == "true" {
+	// 		temp = "rndl.html"
+	// 	}
+	// 	choice, ascen, rndgem := GetNums(num1, numGems)
+	// 	gemImg := strings.Replace(gems[rndgem], " ", "_", -1)
+
+	// 	return c.Render(http.StatusOK, temp, map[string]interface{}{
+	// 		"class":      Classes[choice].Class,
+	// 		"ascendency": Classes[choice].Ascension[ascen],
+	// 		"name":       randomdata.SillyName(),
+	// 		"gemImg":     gemImg,
+	// 		"gem":        gems[rndgem],
+	// 		"league":     randomdata.StringSample("SSF Hardcore Abyss", "Normal Hardcore Abyss", "SSF Softcore Abyss", "Normal Softcore Abyss", "SSF Hardcore Standard", "Normal Hardcore Standard", "SSF Softcore Standard", "Normal Softcore Standard"),
+	// 		// "ssf":        randomdata.StringSample("SSF", "Normal"),
+	// 		// "hc":         randomdata.StringSample("HardCore", "SoftCore"),
+	// 	})
+	// }).Name = "index"
+
+	//e.Logger.Fatal(e.Start(":2086"))
+
+}
